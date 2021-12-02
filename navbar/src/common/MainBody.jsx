885532a5f06a760de75f30cb7c31ca28cd150256
Gus1969 <gustavo.ubalperez@gmail.com>
2021-12-02 17:27:28 -0300

    css added to Cards

--- navbar/src/common/MainBody.jsx
+++ navbar/src/common/MainBody.jsx
@@ -1,43 +1,45 @@
-import React from 'react';
+import React from  'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './MainBody.css';
 import {Container, CardGroup, Card} from 'react-bootstrap';
+import "../App.css";
 
 
 const MainBody = () => {
     return (
+     // const state[show, setShow] = 
         <Container>
         <CardGroup>
   <Card className="CardHolder">
-    <Card.Img className="Picture" variant="top" src="assets/img/pictureOne.jpg"/>
+    <Card.Img className="Picture" variant="top" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"/>
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title className="title">Card title</Card.Title>
       <Card.Text>
         This is a wider card with supporting text below as a natural lead-in to
         additional content. This content is a little bit longer.
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
+      <small className="text-muted"> Footer content</small>
     </Card.Footer>
   </Card>
   <Card className="CardHolder">
-    <Card.Img className="Picture" variant="top" src="holder.js/100px160" />
+    <Card.Img className="Picture" variant="top" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title className="title">Card title</Card.Title>
       <Card.Text>
         This card has supporting text below as a natural lead-in to additional
         content.{' '}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
+      <small className="text-muted">Lorem ipsum dolor sit amet consectetur.</small>
     </Card.Footer>
   </Card>
-  <Card>
-    <Card.Img className="Picture" variant="top" src="holder.js/100px160" />
+  <Card className="CardHolder">
+    <Card.Img className="Picture" variant="top" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title className="title">Card title</Card.Title>
       <Card.Text>
         This is a wider card with supporting text below as a natural lead-in to
         additional content. This card has even longer content than the first to
@@ -45,11 +47,15 @@ const MainBody = () => {
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
+      <small className="text-muted">Footer content</small>
     </Card.Footer>
   </Card>
 </CardGroup>
+<h2>Here starts another section</h2>
 </Container>
+
+
+
 
     )
 }
